db27ef9783e03af0df770906e92c472faf598b3b
BSmotorsport <bsmotorsport2005@hotmail.co.uk>
2025-11-10 18:31:44 +0000

    Delete l2cs directory

--- l2cs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Utilities for running the L2CS gaze estimation model."""
-
-from .pipeline import Pipeline
-from .results import GazeResultContainer
-
-__all__ = ["Pipeline", "GazeResultContainer"]
